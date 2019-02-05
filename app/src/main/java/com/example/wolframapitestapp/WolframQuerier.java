a751ebb9356062bc894cfc2a1e4d03953ab02407
Jacob Pendleton <jdpendleton@student.fullsail.edu>
2019-02-05 10:32:35 -0500

    Fixed encoding bug
    Bug would change queries to wolfram alpha with addition to multiplication

--- app/src/main/java/com/example/wolframapitestapp/WolframQuerier.java
+++ app/src/main/java/com/example/wolframapitestapp/WolframQuerier.java
@@ -6,13 +6,16 @@ import android.os.AsyncTask;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class WolframQuerier extends AsyncTask<String, Void, String> {
+
     String baseURL;
     String appID;
     WolframAPIFetch callback;
 
     public WolframQuerier(Context c) {
+
         callback = (WolframAPIFetch) c;
         baseURL = callback.getBaseURL();
         appID = callback.getAppID();
@@ -22,25 +25,30 @@ public class WolframQuerier extends AsyncTask<String, Void, String> {
 
         String output = "";
         for (String query : params) {
+
             try {
-                URL url = new URL(baseURL + query + appID);
+
+                URL url = new URL(baseURL + URLEncoder.encode(query, "UTF-8") + appID);
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
                 String outLine;
                 while ((outLine = in.readLine()) != null) {
+
                     output += outLine;
                 }
 
                 in.close();
 
             } catch (Exception e) {
+
                 e.printStackTrace();
             }
         }
         return output;
     }
     protected void onPostExecute(String result) {
+
         callback.onEvaluateCompleted(result);
     }
 }
