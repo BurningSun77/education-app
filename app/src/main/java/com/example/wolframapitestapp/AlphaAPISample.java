2c258153bba47d9fd2447e5a325434263fbab457
Jacob Pendleton <jdpendleton@student.fullsail.edu>
2019-02-05 10:08:10 -0500

    Created a starting point for the app
    merged proof of concepts

--- app/src/main/java/com/example/wolframapitestapp/AlphaAPISample.java
+++ app/src/main/java/com/example/wolframapitestapp/AlphaAPISample.java
@@ -18,7 +18,7 @@ public class AlphaAPISample {
 
         // Use "pi" as the default query, or caller can supply it as the lone command-line argument.
         String input = "pi";
-       if (args.length > 0)
+        if (args.length > 0)
            input = args[0];
 
         // The WAEngine is a factory for creating WAQuery objects,
@@ -27,7 +27,7 @@ public class AlphaAPISample {
         // be inherited by all WAQuery objects created from it. Most applications
         // will only need to crete one WAEngine object, which is used throughout
         // the life of the application.
-    WAEngine engine = new WAEngine();
+        WAEngine engine = new WAEngine();
 
         // These properties will be set in all the WAQuery objects created from this WAEngine.
         engine.setAppID(appid);
@@ -40,6 +40,7 @@ public class AlphaAPISample {
         query.setInput(input);
 
         try {
+
             // For educational purposes, print out the URL we are about to send:
             System.out.println("Query URL:");
             System.out.println(engine.toURL(query));
@@ -50,21 +51,29 @@ public class AlphaAPISample {
             WAQueryResult queryResult = engine.performQuery(query);
 
             if (queryResult.isError()) {
+
                 System.out.println("Query error");
                 System.out.println("  error code: " + queryResult.getErrorCode());
                 System.out.println("  error message: " + queryResult.getErrorMessage());
             } else if (!queryResult.isSuccess()) {
+
                 System.out.println("Query was not understood; no results available.");
             } else {
+
                 // Got a result.
                 System.out.println("Successful query. Pods follow:\n");
                 for (WAPod pod : queryResult.getPods()) {
+
                     if (!pod.isError()) {
+
                         System.out.println(pod.getTitle());
                         System.out.println("------------");
                         for (WASubpod subpod : pod.getSubpods()) {
+
                             for (Object element : subpod.getContents()) {
+
                                 if (element instanceof WAPlainText) {
+
                                     System.out.println(((WAPlainText) element).getText());
                                     System.out.println("");
                                 }
@@ -77,6 +86,7 @@ public class AlphaAPISample {
                 // These can be obtained by methods of WAQueryResult or objects deeper in the hierarchy.
             }
         } catch (WAException e) {
+
             e.printStackTrace();
         }
     }
---
+++ app/src/main/java/com/example/wolframapitestapp/WolframQuerier.java
@@ -0,0 +1,46 @@
+package com.example.wolframapitestapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class WolframQuerier extends AsyncTask<String, Void, String> {
+    String baseURL;
+    String appID;
+    WolframAPIFetch callback;
+
+    public WolframQuerier(Context c) {
+        callback = (WolframAPIFetch) c;
+        baseURL = callback.getBaseURL();
+        appID = callback.getAppID();
+    }
+
+    protected String doInBackground(String... params) {
+
+        String output = "";
+        for (String query : params) {
+            try {
+                URL url = new URL(baseURL + query + appID);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                String outLine;
+                while ((outLine = in.readLine()) != null) {
+                    output += outLine;
+                }
+
+                in.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return output;
+    }
+    protected void onPostExecute(String result) {
+        callback.onEvaluateCompleted(result);
+    }
+}
