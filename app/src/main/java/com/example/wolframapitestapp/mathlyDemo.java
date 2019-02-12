184693c73db372fb7514a45142460a8ff3eccee7
Jacob Pendleton <jdpendleton@student.fullsail.edu>
2019-02-12 13:01:50 -0500

    Cleaned up mathlyDemo

--- app/src/main/java/com/example/wolframapitestapp/mathlyDemo.java
+++ app/src/main/java/com/example/wolframapitestapp/mathlyDemo.java
@@ -1,6 +1,5 @@
 package com.example.wolframapitestapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,19 +7,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class mathgeneratordemo extends AppCompatActivity {
+public class mathlyDemo extends AppCompatActivity {
+
     private Button answer1;
     private Button answer2;
     private Button answer3;
     private Button answer4;
+
     private TextView equation;
-    private TextView displaycount;
-    private int wincount;
+    private TextView displayCount;
+
+    private int winCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mathgeneratordemo);
+        setContentView(R.layout.activity_demo);
 
         final MathGenerator mathGenerator = new MathGenerator();
 
@@ -29,38 +31,47 @@ public class mathgeneratordemo extends AppCompatActivity {
         answer2 = findViewById(R.id.button4);
         answer3 = findViewById(R.id.button5);
         answer4 = findViewById(R.id.button6);
+        
         equation = findViewById(R.id.textView2);
-        displaycount = findViewById(R.id.textView3);
+        displayCount = findViewById(R.id.textView3);
 
         answer1.setText(Integer.toString(mathGenerator.getanswer()));
         answer2.setText(Integer.toString(mathGenerator.getf1answer()));
         answer3.setText(Integer.toString(mathGenerator.getf2answer()));
         answer4.setText(Integer.toString(mathGenerator.getf3answer()));
 
-         Intent getscore =getIntent();
+        Intent getScore = getIntent();
 
-         wincount= getscore.getIntExtra("score",0);
+        winCount = getScore.getIntExtra("score",0);
 
         equation.setText((Integer.toString(mathGenerator.getNumber1())+mathGenerator.getoperator()+Integer.toString(mathGenerator.getNumber2())));
-        displaycount.setText("Win Count: "+Integer.toString(wincount));
+        displayCount.setText("Win Count: "+Integer.toString(winCount));
 
         answer1.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
+
                 checkansewer(answer1,mathGenerator);
             }
         });
         answer2.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
+
                 checkansewer(answer2,mathGenerator);
             }
         });
         answer3.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
+
                 checkansewer(answer3,mathGenerator);
             }
         });
         answer4.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
+
                 checkansewer(answer4,mathGenerator);
             }
         });
@@ -73,12 +84,13 @@ public class mathgeneratordemo extends AppCompatActivity {
         int choice = Integer.parseInt(button.getText().toString());
 
         if(choice == mathGenerator.getanswer()){
-            ++wincount;
+
+            winCount++;
         }
 
         finish();
         Intent intent = getIntent();
-        intent.putExtra("score",wincount);
+        intent.putExtra("score", winCount);
         startActivity(getIntent());
     }
 }
