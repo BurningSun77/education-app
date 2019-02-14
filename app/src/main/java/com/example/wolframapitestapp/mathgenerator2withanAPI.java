b3c93af70ca0519320fa6e25305999128d1fb015
Jacob Pendleton <jdpendleton@student.fullsail.edu>
2019-02-14 12:46:22 -0500

    Merge mathgenerator2withanAPI

---
+++ app/src/main/java/com/example/wolframapitestapp/mathgenerator2withanAPI.java
@@ -0,0 +1,124 @@
+package com.example.wolframapitestapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.JsonToken;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import io.github.kexanie.library.MathView;
+
+public class mathgenerator2withanAPI extends AppCompatActivity implements MathMLAPIFetch {
+
+    //private TextView equation;
+    //private TextView choices;
+    private  JSONObject jsonObject;
+    private  MathView mathView;
+    private  MathGeneratorMark2 mathGeneratorMark2;
+    private Button[] choices = new Button[5];
+    private int wincount;
+    private TextView displaycount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mathgenerator2withan_api);
+
+        //equation = findViewById(R.id.textView5);
+        //choices = findViewById(R.id.textView5);
+        mathView = findViewById(R.id.formula_one);
+        // new MathMLQuerier(this).execute("");
+
+
+        choices[0] = findViewById(R.id.button8);
+        choices[1] = findViewById(R.id.button9);
+        choices[2] = findViewById(R.id.button10);
+        choices[3] = findViewById(R.id.button11);
+        choices[4] = findViewById(R.id.button12);
+        displaycount = findViewById(R.id.textView6);
+
+
+        // int start=8;
+        // for (int i=0; i<5;++i){
+
+        //     choices[i] = findViewById(R.id.button+start);
+        //     start++;
+        // }
+
+
+        // try {
+        //     equation.setText(jsonObject.getString("question"));
+        // } catch (JSONException e) {
+        //     e.printStackTrace();
+        // }
+
+        MathMLQuerier mq = new MathMLQuerier(this);
+        mq.execute("https://math.ly/api/v1/algebra/linear-equations.json");
+
+
+
+
+    }
+
+    @Override
+    public void onEvaluateCompleted(String result) {
+       try {
+           jsonObject = new JSONObject(result);
+           mathGeneratorMark2 = new MathGeneratorMark2(jsonObject);
+           mathView.setText(jsonObject.getString("question"));
+           Intent getscore =getIntent();
+           wincount= getscore.getIntExtra("score",0);
+           displaycount.setText("Win Count: "+Integer.toString(wincount));
+           mathGeneratorMark2.setWincount(wincount);
+           for(int i=0;i<choices.length;++i){
+               choices[i].setText(mathGeneratorMark2.getNumber(i));
+               final int finalI = i;
+               choices[i].setOnClickListener(new View.OnClickListener() {
+                   public void onClick(View v) {
+                       checkansewer(finalI,mathGeneratorMark2);
+                   }
+               });
+           }
+
+
+         // equation.setText(mathGeneratorMark2);
+           //JSONArray jsonArray = jsonObject.getJSONArray("choices");
+           // int answer = Integer.parseInt( jsonObject.getString("correct_choice"));
+          //JSONArray jsonArray = jsonObject.getJSONArray("choices");
+          //equation.setText(jsonObject.getString("question"));
+          //choices.setText(jsonArray.toString());
+
+
+
+
+       } catch (JSONException e) {
+           e.printStackTrace();
+       }
+   }
+
+
+    public void checkansewer(int i,MathGeneratorMark2 mathGeneratorMark2){
+
+
+        mathGeneratorMark2.checkanser(i);
+        wincount = mathGeneratorMark2.getWincount();
+        finish();
+        Intent intent = getIntent();
+        intent.putExtra("score",wincount);
+        startActivity(getIntent());
+    }
+}
